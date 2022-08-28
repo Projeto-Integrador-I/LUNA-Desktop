13599104dae911d2a7083e191943cee9e86c8225
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-08-28 12:38:12 -0300

    Correção de Scripts e Adicionado Favicon

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "./src/assets/favicon.ico",
   });
 
-  win.loadFile("@/pages/Login/index.html");
+  win.loadFile("./src/pages/Login/index.html");
 }
 
 app.on("ready", createWindow);
