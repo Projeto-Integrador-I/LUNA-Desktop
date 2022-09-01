f5449d57702a757a481d1ebbe640b56c4e6f23e7
Gabriel Dullius <gd@interact.com.br>
2022-09-01 13:25:41 -0300

    refactor: aligned contents and search bar animation

--- src/pages/Home/javascript/script.js
+++ src/pages/Home/javascript/script.js
@@ -1,8 +1,29 @@
 const searchButton = document.getElementById('searchButton');
+const searchBar = document.getElementById('searchBar');
 
-searchButton.addEventListener('click', button =>
+searchButton.addEventListener('click', () =>
 {
-    button.target.style.display = 'none';
+    openSearchBar();
+});
 
-    
-})
+window.addEventListener('click', div =>
+{
+    let divId = div.target.id;
+
+    if( divId != 'searchButton' && divId != 'searchInput' && divId != 'searchIcon' )
+    {
+        closeSearchBar();
+    }
+});
+
+function openSearchBar()
+{
+    searchButton.style.display = 'none';
+    searchBar.style.display = 'flex';
+}
+
+function closeSearchBar()
+{
+    searchBar.style.display = 'none';
+    searchButton.style.display = 'flex';
+}
