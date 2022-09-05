2900fb2999d22f205633b6cc0f105723c0f690e8
César Augusto Matiello Zanotelli <110305935+CesarZanotelli1@users.noreply.github.com>
2022-09-04 21:44:05 -0300

    Navegação entre páginas e Login

--- src/pages/Login/javascript/main.js
+++ src/pages/Login/javascript/main.js
@@ -1,6 +1,16 @@
-const button = document.getElementById("enter");
-const inputs = document.getElementsByClassName("input");
+const enter_btn = window.document.querySelector('button.enter')
+enter_btn.addEventListener('click', enter)
+function enter(event){
+    event.preventDefault()
 
-button.onclick = () => {
-  alert("Hello World");
-};
+    let inputUser = document.querySelector('#login').value
+    let inputPass = document.querySelector('#pass').value
+    console.log(inputUser,inputPass)
+    if (inputUser == 'admin' && inputPass == 'admin'){
+        //window.location.href ="../Home/home.html"
+
+//    alert("Agora eu tenho uma função");
+    } else {
+        window.alert ("Usuário ou senha incorreto!")
+    }
+}
