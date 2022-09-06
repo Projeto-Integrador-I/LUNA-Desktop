c9b8c5bb60504adf06b6ca3e6340ab3447015a1c
Fernando H. Filter <ff@interact.com.br>
2022-09-06 08:36:33 -0300

    Alguns ajustes

--- src/pages/Login/javascript/main.js
+++ src/pages/Login/javascript/main.js
@@ -1,16 +1,17 @@
 const enter_btn = window.document.querySelector('button.enter')
 enter_btn.addEventListener('click', enter)
-function enter(event){
+
+function enter(event) {
     event.preventDefault()
 
     let inputUser = document.querySelector('#login').value
     let inputPass = document.querySelector('#pass').value
-    console.log(inputUser,inputPass)
-    if (inputUser == 'admin' && inputPass == 'admin'){
+
+    if (inputUser == 'admin' && inputPass == 'admin') {
         //window.location.href ="../Home/home.html"
 
-//    alert("Agora eu tenho uma função");
+        //    alert("Agora eu tenho uma função");
     } else {
-        window.alert ("Usuário ou senha incorreto!")
+        window.alert("Usuário ou senha incorreto!")
     }
 }
