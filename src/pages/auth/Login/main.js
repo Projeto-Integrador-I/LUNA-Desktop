7c6074357faa94e6726ea652c82b84635ab45462
Ottenim <gabriel.minetto@universo.univates.br>
2022-09-10 15:51:14 -0300

    TokenResponse

--- src/pages/auth/Login/main.js
+++ src/pages/auth/Login/main.js
@@ -1,11 +1,9 @@
 const enter_btn = window.document.querySelector('button.enter')
-enter_btn.addEventListener('click', enter)
-
-//import { getUserToken } from '@/Controllers/userController.js'
-//import { User } from '@/Axios/user.js'
-//url='http://26.2.1.64:8080/luna/'
-
 enter_btn.addEventListener('click', authenticate)
+
+
+const url='http://26.2.1.64:8080/luna/'
+
 
 function authenticate(event) {
     event.preventDefault()
@@ -16,30 +14,17 @@ function authenticate(event) {
     console.log(user.name);
     console.log(user.password);
 
-    const userJson = JSON.parse('{"username":"' + user.name + '","password":"' + user.password + '"}')
-
-    console.log(userJson);
-
-    axios.post(url + 'authenticate', userJson)
+    axios.post(url + 'authenticate', {
+        username: user.name,
+        password: user.password
+    })
          .then(res => { 
-             console.log(res); 
+             const token = res.data.token
+             console.log(token); 
          })
          .catch(err => console.log(err))
-
-    if (getUserToken(user)) {
-        
-        console.log("autenticação feita com sucesso")
-    } else {
-        window.alert("Usuário ou senha incorreto!")
-    }        
+      
 }
-
-function getUserToken(user){
-    axios.get(url+authenticate,JSON.parse())
-    .then(res =>  { console.log(res); } )
-    .catch(err => console.log(err))
-}
-
 class User{
     constructor(name,password) {
         this.name = name
