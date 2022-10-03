b55d869b7107951c1b1c88a20617728d4b7af5af
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-10-02 21:44:30 -0300

    Criado novo Footer

--- src/components/v-footer.js
+++ src/components/v-footer.js
@@ -3,30 +3,55 @@ const footer = document.createElement('template');
 css = `
 .footer {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
   width: 100vw;
-  height: 40vh;
+  height: 35vh;
   margin-top: 30px;
 
   background: #070707;
 }
 
-.footer_logo {
-  padding-right: 60%;
+.footer_options {
+  display: flex;
+  align-items: row;
+  list-style: none;
+  color: #757575;
+  gap: 21px;
+  font-size: 16pt;
 }
 
-.footer_content {
+.footer_socials {
   display: flex;
-  flex-direction: column;
+  list-style: none;
+  margin: 15px;
+  gap: 50px;
+  padding: 0;
+}
+
+.footer_social {
+  background-color: #fff;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.footer span {
-  padding: 10px;
+.footer_social:hover {
+  cursor: pointer;
+}
+
+.footer_option:hover {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.footer_content {
+  padding-top: 15px;
   color: #fff;
 }`
 
@@ -36,10 +61,41 @@ footer.innerHTML = `
 </style>
 
 <div class="footer">
-  <img src="../../../assets/logo.png" alt="Luna Logo" class="footer_logo">
+  <img src="../../../assets/footer_logo.png" alt="Luna Logo" class="footer_logo">
+  <div>
+    <ul class="footer_options">
+      <li class="footer_option">Sobre</li>
+      <li class="footer_option">Contato</li>
+      <li class="footer_option">Termos de Uso</li>
+      <li class="footer_option">Política de Privacidade</li>
+    </ul>
+  </div>
+  <div>
+    <ul class="footer_socials">
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/facebook_logo.png" alt="facebook">
+        </div>
+      </li>
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/insta_logo.png" alt="instagram">
+        </div>
+      </li>
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/twitter_logo.png" alt="twitter">
+        </div>
+      </li>
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/github_logo.png" alt="github">
+        </div>
+      </li>
+    </ul>
+  </div>
   <div class="footer_content">
-    <span>&copy; Luna</span>
-    <span>Todos direitos reservados</span>
+    <span>Copyright 2022 &copy; LUNA. Todos os direitos reservados</span>
   </div>
 </div>`;
 
@@ -50,4 +106,5 @@ class VFooter extends HTMLElement {
     this.shadowRoot.appendChild(footer.content.cloneNode(true));
   }
 }
+
 window.customElements.define('v-footer', VFooter);
