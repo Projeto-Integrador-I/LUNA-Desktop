c1795a3199fb90f781ffb2c758ef55bbfe5f6431
César Augusto Matiello Zanotelli <110305935+CesarZanotelli1@users.noreply.github.com>
2022-10-12 20:05:22 -0300

    Revert "Merge branch 'main' of https://github.com/Projeto-Integrador-I/LUNA-Desktop"
    This reverts commit e0cc8f6628863bd83f2b5f18a57917e12c198c36, reversing
    changes made to f671896391eb40ad39747c459c300b21a271f8f3.

--- src/components/v-header.js
+++ src/components/v-header.js
@@ -75,7 +75,7 @@ header.innerHTML = `
       <li class="menu-option"><a href="../search/index.html">Pesquisas</a></li>
   </ul>
   <div class="perfil">
-      <span>Admin</span>
+      <span>AAAAA</span>
       <img src="../../../assets/User.svg" alt="user-icon" srcset="">
   </div>
 </div>`;
