4420e4bc0cbeac4749d1dab255956c806ee140e5
Ottenim <gabriel.minetto@universo.univates.br>
2022-10-20 10:04:30 -0300

    bannerNcard fixes

--- src/components/v-banner.js
+++ src/components/v-banner.js
@@ -1,14 +1,14 @@
 import instance from '../services/axiosInstace.js'
 
-  instance.get('movies?name=velozes+furiosos')  
+  instance.get('most-played')  
     .then((res) => {
         const setMovie = res.data[Math.floor(Math.random() * res.data.length - 1)];
         var banner = document.getElementById("banner");
         var banner_title = document.getElementById("banner__title");
         //var banner_desc = document.getElementById("banner_description");
        // banner.style.backgroundImage = "url(" + setMovie.belongsToCollection.backdrop_path + ")";
-        banner.style.backgroundImage = "url(" + setMovie.posterPath + ")";
+        banner.style.backgroundImage = "url(" + setMovie.coverLink + ")";
         //banner_desc.innerText = ("OVERVIEW");
-        banner_title.innerText = setMovie.title
+        banner_title.innerText = setMovie.name;
     }
     );
