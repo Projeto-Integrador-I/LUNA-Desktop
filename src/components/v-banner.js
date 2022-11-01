6a59469fda7f6d6340a3ac1021dab0d279c3b0d9
Ottenim <gabriel.minetto@universo.univates.br>
2022-11-01 19:40:53 -0300

    new Banner without requests

--- src/components/v-banner.js
+++ src/components/v-banner.js
@@ -1,14 +1,86 @@
-import instance from '../services/axiosInstace.js'
 
-  instance.get('most-played')  
-    .then((res) => {
-        const setMovie = res.data[Math.floor(Math.random() * res.data.length - 1)];
-        var banner = document.getElementById("banner");
-        var banner_title = document.getElementById("banner__title");
-        //var banner_desc = document.getElementById("banner_description");
-       // banner.style.backgroundImage = "url(" + setMovie.belongsToCollection.backdrop_path + ")";
-        banner.style.backgroundImage = "url(" + setMovie.coverLink + ")";
-        //banner_desc.innerText = ("OVERVIEW");
-        banner_title.innerText = setMovie.name;
+    const banner = document.getElementById("banner");
+    const slides = document.createElement("div");
+    const nav_auto = document.createElement("div");
+    const manual_nav = document.createElement("div");
+
+    slides.setAttribute("class","slides");
+
+    nav_auto.setAttribute("class","navigation-auto");
+
+    manual_nav.setAttribute("class","manual-navigation");
+    
+    for (let i = 1; i < 5; i++) {
+        const input = document.createElement("input");
+        
+        input.setAttribute("type","radio")
+        input.setAttribute("name","radio-btn");
+        input.setAttribute("id","radio"+i);  
+        
+        slides.appendChild(input);  
+        
     }
-    );
+    
+
+    for (let i = 1; i < 5; i++) {
+      const slide = document.createElement("div");
+      const img = document.createElement("img");
+
+      if( i == 1 ){
+          slide.setAttribute("class","slide first");
+        } else {
+          slide.setAttribute("class","slide");
+        }
+        img.setAttribute("src","../../../assets/homeSlider/banner"+i+""+i+".jpg");
+
+        slide.appendChild(img);
+
+        slides.appendChild(slide);
+    }
+
+    banner.appendChild(slides);
+
+    for (let i = 1; i < 5; i++) {
+       const auto_btn = document.createElement("div");
+
+       auto_btn.setAttribute("class","auto-btn"+i);
+
+       nav_auto.appendChild(auto_btn);
+       
+    }
+    
+    banner.appendChild(nav_auto);
+
+    for (let i = 1; i < 5; i++) {
+        const manual_btn = document.createElement("label");
+        manual_btn.setAttribute("for","radio"+i);
+        manual_btn.setAttribute( "class","manual-btn");
+        
+
+        manual_nav.appendChild(manual_btn);   
+        console.log(banner); 
+    }
+    
+    banner.appendChild(manual_nav);
+     
+
+let count = 1;
+
+setInterval(function(){
+    
+    if(count > 1){
+        document.getElementsByClassName('auto-btn' + (count - 1))[0].style.backgroundColor = "transparent";
+    } 
+    if( count != 4 ){
+        document.getElementsByClassName('auto-btn4')[0].style.backgroundColor = "transparent";
+    }
+   
+    document.getElementById('radio' + count).checked = true;
+    document.getElementsByClassName('auto-btn' + count)[0].style.backgroundColor = "#ffffff";
+   
+    count++;
+    if(count>4){
+        
+        count = 1;
+    }
+},5000);
