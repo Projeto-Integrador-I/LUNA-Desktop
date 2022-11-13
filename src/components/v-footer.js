115a2b6d40d3bf523bc61f8259022bf24af9990c
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-11-13 01:09:00 -0300

    Criação do Rodapé com JS DOM

--- src/components/v-footer.js
+++ src/components/v-footer.js
@@ -1,93 +1,6 @@
-const footer = document.getElementById('footer')
-footer.setAttribute('class', 'footer')
+const footer = document.createElement('template');
 
-const footerLogo = document.createElement('img')
-footerLogo.setAttribute('src', '../../../assets/footer_logo.png')
-footerLogo.setAttribute('class', 'footer_logo')
-
-// Pages Nav ----------------------------------------------------
-const pagesDiv = document.createElement('div')
-
-const pagesList = document.createElement('ul')
-pagesList.setAttribute('class', 'footer_options')
-
-const pagesItems = [
-  { href: 'About/index.html', text: 'Sobre' },
-  { href: 'Contact/index.html', text: 'Contato' },
-  { href: 'TermsOfUse/index.html', text: 'Termos de Uso' },
-  { href: 'PrivacyPolicy/index.html', text: 'Política de Privacidade' }
-]
-
-let pageItem, pageLink
-
-pagesItems.forEach(item => {
-  pageItem = document.createElement('li')
-  pageLink = document.createElement('a')
-
-  pageLink.setAttribute('class', 'footer_option')
-
-  pageLink.href = `../${item.href}`
-  pageLink.innerHTML = item.text
-
-  pageItem.appendChild(pageLink)
-  pagesList.appendChild(pageItem)
-})
-
-pagesDiv.append(pagesList)
-// --------------------------------------------------------------
-
-// Social Medias ------------------------------------------------
-const socialsDiv = document.createElement('div')
-
-const socialsList = document.createElement('ul')
-socialsList.setAttribute('class', 'footer_socials')
-
-const socialMedias = [
-  { href: '', name: 'facebook' },
-  { href: '', name: 'instagram' },
-  { href: '', name: 'twitter' },
-  { href: '', name: 'github' }
-]
-
-let socialMedia, socialDiv, socialIcon
-
-socialMedias.forEach(social => {
-  socialMedia = document.createElement('li')
-  socialDiv = document.createElement('div')
-  socialIcon = document.createElement('img')
-
-  socialDiv.setAttribute('class', 'footer_social')
-
-  socialIcon.setAttribute('src', `../../../assets/medias/${social.name}_logo.png`)
-
-  socialDiv.appendChild(socialIcon)
-  socialMedia.appendChild(socialDiv)
-  socialsList.appendChild(socialMedia)
-})
-
-socialsDiv.appendChild(socialsList)
-// --------------------------------------------------------------
-
-// Content ------------------------------------------------------
-const contentDiv = document.createElement('div')
-contentDiv.setAttribute('class', 'footer_content')
-
-const copyright = document.createElement('span')
-
-copyright.innerHTML = 'Copyright 2022 &copy; LUNA. Todos os direitos reservados.'
-
-contentDiv.appendChild(copyright)
-// --------------------------------------------------------------
-
-footer.appendChild(footerLogo)
-footer.appendChild(pagesDiv)
-footer.appendChild(socialsDiv)
-footer.appendChild(contentDiv)
-
-// Style --------------------------------------------------------
-const style = document.createElement('style')
-
-style.innerHTML = `
+css = `
 .footer {
   display: flex;
   flex-direction: column;
@@ -101,10 +14,6 @@ style.innerHTML = `
   background: #070707;
 }
 
-.footer_logo {
-  padding: 0;
-}
-
 .footer_options {
   display: flex;
   align-items: row;
@@ -112,19 +21,6 @@ style.innerHTML = `
   color: #757575;
   gap: 21px;
   font-size: 16pt;
-}
-
-.footer_option {
-  padding: 4px;
-  color: white;
-  text-decoration: none;
-  border-radius: 5px;
-  transition: background-color 0.3s ease-out 5ms
-}
-
-.footer_option:hover {
-  cursor: pointer;
-  background-color: #ffffff1c;
 }
 
 .footer_socials {
@@ -149,11 +45,66 @@ style.innerHTML = `
   cursor: pointer;
 }
 
-.footer_content {
-  color: #fff;
-  padding-top: 15px;
+.footer_option:hover {
+  cursor: pointer;
+  text-decoration: underline;
 }
-`
 
-document.head.appendChild(style)
-// --------------------------------------------------------------
+.footer_content {
+  padding-top: 15px;
+  color: #fff;
+}`
+
+footer.innerHTML = `
+<style>
+  ${css}
+</style>
+
+<div class="footer">
+  <img src="../../../assets/footer_logo.png" alt="Luna Logo" class="footer_logo">
+  <div>
+    <ul class="footer_options">
+      <li class="footer_option">Sobre</li>
+      <li class="footer_option">Contato</li>
+      <li class="footer_option">Termos de Uso</li>
+      <li class="footer_option">Política de Privacidade</li>
+    </ul>
+  </div>
+  <div>
+    <ul class="footer_socials">
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/medias/facebook_logo.png" alt="facebook">
+        </div>
+      </li>
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/medias/insta_logo.png" alt="instagram">
+        </div>
+      </li>
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/medias/twitter_logo.png" alt="twitter">
+        </div>
+      </li>
+      <li>
+        <div class="footer_social">
+          <img src="../../../assets/medias/github_logo.png" alt="github">
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div class="footer_content">
+    <span>Copyright 2022 &copy; LUNA. Todos os direitos reservados</span>
+  </div>
+</div>`;
+
+class VFooter extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(footer.content.cloneNode(true));
+  }
+}
+
+window.customElements.define('v-footer', VFooter);
