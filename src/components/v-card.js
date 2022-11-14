4014a445890899ab78b2509c593eda22b29e590b
Ottenim <gabriel.minetto@universo.univates.br>
2022-11-14 20:33:20 -0300

    v-card update

--- src/components/v-card.js
+++ src/components/v-card.js
@@ -1,13 +1,84 @@
 import instance from '../services/axiosInstace.js';
 
-instance.get('most-played',
-{ headers: { "Content-Type": "application/json" }}) 
-.then((res) => {
+let allTvShows, allGames, allMovies;
+
+if( allTvShows == null && allGames == null && allMovies == null){
+  let mostPlayed = async () => {
+    const games = await instance.get('most-played',
+        { headers: { "Content-Type": "application/json" }}) 
+        .then((res) => { return res.data; })
+        .catch((err) => { console.log(err) })
+        return games;
+  }
+
+  let shows = async () => {
+    const trendingTv =  await instance.get('trending-tv',
+        { headers: { "Content-Type": "application/json" }})  
+        .then((res) => { return res.data })
+        .catch((err) => { console.log(err) })
+        return trendingTv;
+  }
+
+  let movies = async () => {
+    const trendingMovies = await instance.get('trending-movies',
+        { headers: { "Content-Type": "application/json" }}) 
+        .then((res) => { return res.data })
+        .catch((err) => { console.log(err) })
+        return trendingMovies;
+  }
+
+  shows().then((res) => {
+    allTvShows = res;
+    constructCardShow(allTvShows);
+  })
+  .then(mostPlayed().then((res) => {
+    allGames = res;
+    constructCardGames(allGames);
+  }))
+  .then(movies().then((res) => {
+    allMovies = res;
+    constructCardMovies(allMovies);
+  }));
+}
+
+//Não usar um construtor pois os dados vêm de maneira diferente
+/*
+function constructCard(list) {
+  //criar as faixas
+  
+  list.forEach(midia => {
+    console.log(midia);
+    
+  })
+}
+
+/*
+<div class="modal">
+        <div class="modal_title"> name </div>
+        <div class="modal_info">
+            <img class="modal_poster" src="coverLink" alt="não precisa">
+            <div class="modal_specific-info">
+                <h4> aboutGame ou desc ou developers[] / releaseDate / publishers[] <h4>
+            </div>
+        </div>
+        <div class="modal_extra-info">
+            <h4> aboutGame ou desc <h4>
+        </div>
+        <div class="modal_div-button">
+            <button class="modal_add-to-list">Adicionar a lista</button>
+            <button class="modal_return"></button>
+        </div>
+        
+    </div>
+    
+*/
+
+function constructCardGames(midia){
 
   const headrow = document.getElementById("headrow");
   const row = document.createElement("div");
   row.className = "row";
-  row.classList.add("booksrow");
+  row.classList.add("gamesrow");
   headrow.appendChild(row);
   const title = document.createElement("h2");
   title.className = "row__title";
@@ -17,7 +88,7 @@ instance.get('most-played',
   row_posters.className = "row__posters";
   row.appendChild(row_posters);
 
-  res.data.forEach(game => {
+  midia.forEach(game => {
     if(game != null){
       const poster = document.createElement("img");
 
@@ -29,16 +100,14 @@ instance.get('most-played',
       row_posters.appendChild(poster);
     }
   });
-});
+};
 
-instance.get('trending-tv',
-  { headers: { "Content-Type": "application/json" }})  
-.then((res) => {
-
+function  constructCardShow(midia) {
+  
   const headrow = document.getElementById("headrow");
   const row = document.createElement("div");
   row.className = "row";
-  row.classList.add("gamesrow");
+  row.classList.add("tvShowrow");
   headrow.appendChild(row);
   const title = document.createElement("h2");
   title.className = "row__title";
@@ -48,7 +117,7 @@ instance.get('trending-tv',
   row_posters.className = "row__posters";
   row.appendChild(row_posters);
 
-  res.data.forEach(tv => {
+  midia.forEach(tv => {
 
     const poster = document.createElement("img");
     poster.className = "row__posterLarge";
@@ -58,13 +127,9 @@ instance.get('trending-tv',
     row_posters.appendChild(poster);
 
   });
-});
+};
 
-
-instance.get('trending-movies',
-{ headers: { "Content-Type": "application/json" }}) 
-.then((res) => {
-
+function constructCardMovies(midia) {
   const headrow = document.getElementById("headrow");
   const row = document.createElement("div");
   row.className = "row";
@@ -78,45 +143,13 @@ instance.get('trending-movies',
   row_posters.className = "row__posters";
   row.appendChild(row_posters);
 
-  res.data.forEach(movie => {
+  midia.forEach(movie => {
+    console.log(movie);
     const poster = document.createElement("img");
+    // poster.appId = movie.id;
     poster.className = "row__posterLarge";
     poster.id = movie.title;
     poster.src = movie.coverLink;
     row_posters.appendChild(poster);
   });
-});
-/*
-instance.get('book?name=o+pequeno+princepe',
-{ headers: { "Content-Type": "application/json" }}) 
-.then((res) => {
-console.log(res);
-const headrow = document.getElementById("headrow");
-const row = document.createElement("div");
-row.className = "row";
-row.classList.add("booksrow");
-headrow.appendChild(row);
-const title = document.createElement("h2");
-title.className = "row__title";
-title.innerText = "Books";
-row.appendChild(title);
-const row_posters = document.createElement("div");
-row_posters.className = "row__posters";
-row.appendChild(row_posters);
-
-res.data.forEach(book => {
-
-if(!book.imageLinks.length == 0){
-
-const poster = document.createElement("img");
-
-poster.className = "row__posterLarge";
-poster.id = book.title;
-
-poster.src = book.imageLinks[0];
-
-row_posters.appendChild(poster);
-} 
-});
-});
-*/
+};
