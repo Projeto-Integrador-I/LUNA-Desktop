123e92163dfa0511c1fa1439e9cef799343c8993
Ottenim <gabriel.minetto@universo.univates.br>
2022-11-17 18:45:37 -0300

    modal page home

--- src/components/v-card.js
+++ src/components/v-card.js
@@ -51,26 +51,6 @@ function constructCard(list) {
     
   })
 }
-
-/*
-<div class="modal">
-        <div class="modal_title"> name </div>
-        <div class="modal_info">
-            <img class="modal_poster" src="coverLink" alt="não precisa">
-            <div class="modal_specific-info">
-                <h4> aboutGame ou desc ou developers[] / releaseDate / publishers[] <h4>
-            </div>
-        </div>
-        <div class="modal_extra-info">
-            <h4> aboutGame ou desc <h4>
-        </div>
-        <div class="modal_div-button">
-            <button class="modal_add-to-list">Adicionar a lista</button>
-            <button class="modal_return"></button>
-        </div>
-        
-    </div>
-    
 */
 
 function constructCardGames(midia){
@@ -90,13 +70,16 @@ function constructCardGames(midia){
 
   midia.forEach(game => {
     if(game != null){
+      
       const poster = document.createElement("img");
-
+      
       poster.className = "row__posterLarge";
       poster.id = game.name;
 
-      poster.src = game.coverLink;
+      poster.gameId = game.apiId;
 
+      poster.src = game.coverLink;
+      
       row_posters.appendChild(poster);
     }
   });
@@ -118,11 +101,12 @@ function  constructCardShow(midia) {
   row.appendChild(row_posters);
 
   midia.forEach(tv => {
-
+    
     const poster = document.createElement("img");
     poster.className = "row__posterLarge";
     poster.id = tv.title;
     poster.src = tv.coverLink;
+    poster.tvId = tv.apiId;
     
     row_posters.appendChild(poster);
 
@@ -144,12 +128,171 @@ function constructCardMovies(midia) {
   row.appendChild(row_posters);
 
   midia.forEach(movie => {
-    console.log(movie);
+    
     const poster = document.createElement("img");
-    // poster.appId = movie.id;
+    
     poster.className = "row__posterLarge";
     poster.id = movie.title;
     poster.src = movie.coverLink;
+    poster.movieId = movie.apiId;
+
     row_posters.appendChild(poster);
   });
 };
+
+
+// class Modal{
+// constructor(){
+//   var modal = document.createElement('div');
+//   modal.className = 'modal_midia';
+
+//   var modal_title = document.createElement('div');
+//   modal_title.className = 'modal_title';
+  
+//   var modal_info = document.createElement('div');
+//   modal_info.className = 'modal_info';
+  
+//   var modal_poster = document.createElement('img');
+//   modal_poster.className = 'modal_poster';
+
+//   var modal_specific_info = document.createElement('div');
+//   modal_specific_info.className = 'modal_specific-info';
+
+//   var gameinfo = document.createElement('h4');
+
+//   modal_specific_info.appendChild(gameinfo);
+//   modal_info.appendChild(modal_poster);
+//   modal_info.appendChild(modal_specific_info);
+
+//   var modal_extra_info = document.createElement('div');
+//   modal_extra_info.className = 'modal_extra-info';
+
+//   var gameExtra = document.createElement('h4');
+
+//   modal_extra_info.appendChild(gameExtra);
+
+//   var modal_btns = document.createElement('div');
+//   modal_btns.className = 'modal_div-button';
+
+//   var add = document.createElement('button');
+//   add.className = 'modal_add-to-list';
+//   add.innerHTML = 'Adicionar a lista';
+
+//   var back = document.createElement('button');
+//   back.className = 'modal_return';
+
+//   modal_btns.appendChild(add);
+//   modal_btns.appendChild(back);
+
+  
+//   modal.appendChild(modal_title);
+//   modal.appendChild(modal_info);
+//   modal.appendChild(modal_extra_info);
+//   modal.appendChild(modal_btns);
+//   createModal()
+// }
+
+// setTitle(title){
+//   this.modal_title.innerHTML = title;
+// }
+
+// }
+
+// let m = new Modal();
+const modal = document.getElementById('modal_midia');
+const modal_title = document.getElementById('modal_title');
+const modal_poster = document.getElementById('modal_poster');
+const modal_add = document.getElementById('modal_add-to-list');
+const modal_return = document.getElementById('modal_return');
+const info1 = document.getElementById('box_info_1');
+const info2 = document.getElementById('box_info_2');
+/*
+  <div class="modal_title"> name </div>
+  <div class="modal_info">
+      <img class="modal_poster" src="coverLink">
+      <div class="modal_specific-info">
+          <h4> aboutGame ou desc ou developers[] / releaseDate / publishers[] <h4>
+      </div>
+  </div>
+  <div class="modal_extra-info">
+      <h4> aboutGame ou desc <h4>
+  </div>
+  <div class="modal_div-button">
+      <button class="modal_add-to-list">Adicionar a lista</button>
+      <button class="modal_return"></button>
+  </div>
+*/
+
+window.onclick = (event) => {
+  const midia = event.target;
+  let id;
+  if (midia.movieId != null) {
+    id = midia.movieId;
+
+    allMovies.forEach(movie => {
+      if( id == movie.apiId ){
+
+        modal_title.innerHTML = movie.title;
+        modal_poster.src = movie.coverLink;
+        //arrumar essa parte
+        info1.innerHTML = 'Release Date: ' + movie.releaseDate +
+                          '               Genres: ' + movie.genres + 
+                          '               Production Companies: ' + movie.productionCompanies;
+        info2.innerHTML = movie.overView;
+        modal.style.display = "block";
+      }   
+    })
+
+  } else if(midia.gameId != null){
+    id = midia.gameId;
+
+    allGames.forEach(game => {
+      if( id == game.apiId ){
+        modal_title.innerHTML = game.name;
+        modal_poster.src = game.coverLink;
+        //arrumar essa parte
+        info1.innerHTML = 'Release Date: ' + game.releaseDate +
+                          '               Developers: ' + game.developers + 
+                          '               Publishers: ' + game.publishers;        ;
+        info2.innerHTML =  game.desc;  
+        modal.style.display = "block";
+       }
+    })
+  } else if(midia.tvId != null){
+      id = midia.tvId
+
+      allTvShows.forEach(tv => {
+        if( id == tv.apiId ){
+          modal_title.innerHTML = tv.title;
+          modal_poster.src = tv.coverLink;
+          //arrumar essa parte
+          info1.innerHTML = 'Genres: ' + tv.genres + 
+                            '               Production Companies: ' + tv.productionCompanies;
+          info2.innerHTML = tv.overView;
+            // console.log(tv);
+          modal.style.display = "block";
+        }
+      })
+
+  }else if (midia == modal){
+    modal.style.display ="block";
+  }   else {
+    modal.style.display = "none";
+  }
+  
+}
+
+
+modal_return.onclick = () => {
+  modal.style.display = "none"
+}
+
+//Arrumar display
+
+
+
+
+
+
+
+
--- src/pages/app/Home/script.js
+++
@@ -1,3 +0,0 @@
-// import { User } from "../../../model/user.js"
-
-// console.log(User.getId);
